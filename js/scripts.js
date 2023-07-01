13b5111ccbd5fff9fed1ffe9f8714b9944708082
CodingAly <134792361+CodingAly@users.noreply.github.com>
2023-07-02 00:58:02 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,257 @@
+/* Description: Custom JS file */
+
+/* Navigation */
+// Collapse the navbar by adding the top-nav-collapse class
+window.onscroll = function () {
+	scrollFunction();
+	scrollFunctionBTT(); // back to top button
+};
+
+window.onload = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.documentElement.scrollTop > 30) {
+		document.getElementById("navbarExample").classList.add("top-nav-collapse");
+	} else if ( document.documentElement.scrollTop < 30 ) {
+		document.getElementById("navbarExample").classList.remove("top-nav-collapse");
+	}
+}
+
+// Navbar on mobile
+let elements = document.querySelectorAll(".navbar-nav .nav-link:not(.dropdown-toggle)");
+
+for (let i = 0; i < elements.length; i++) {
+	elements[i].addEventListener("click", () => {
+		document.querySelector(".offcanvas-collapse").classList.toggle("open");
+	});
+}
+
+document.querySelector(".navbar-toggler").addEventListener("click", () => {
+  	document.querySelector(".offcanvas-collapse").classList.toggle("open");
+});
+
+// Hover on desktop
+function toggleDropdown(e) {
+	const _d = e.target.closest(".dropdown");
+	let _m = document.querySelector(".dropdown-menu", _d);
+
+	setTimeout(
+		function () {
+		const shouldOpen = _d.matches(":hover");
+		_m.classList.toggle("show", shouldOpen);
+		_d.classList.toggle("show", shouldOpen);
+
+		_d.setAttribute("aria-expanded", shouldOpen);
+		},
+		e.type === "mouseleave" ? 300 : 0
+	);
+}
+
+// On hover
+const dropdownCheck = document.querySelector('.dropdown');
+
+if (dropdownCheck !== null) { 
+	document.querySelector(".dropdown").addEventListener("mouseleave", toggleDropdown);
+	document.querySelector(".dropdown").addEventListener("mouseover", toggleDropdown);
+
+	// On click
+	document.querySelector(".dropdown").addEventListener("click", (e) => {
+		const _d = e.target.closest(".dropdown");
+		let _m = document.querySelector(".dropdown-menu", _d);
+		if (_d.classList.contains("show")) {
+			_m.classList.remove("show");
+			_d.classList.remove("show");
+		} else {
+			_m.classList.add("show");
+			_d.classList.add("show");
+		}
+	});
+}
+
+
+/* Rotating Text - ReplaceMe */
+var checkReplace = document.querySelector('.replace-me');
+if (checkReplace !== null) { 
+	var replace = new ReplaceMe(document.querySelector('.replace-me'), {
+		animation: 'animated fadeIn',                       // Animation class or classes
+		speed: 2000,                                        // Delay between each phrase in miliseconds
+		separator: ',',                                     // Phrases separator
+		hoverStop: false,                                   // Stop rotator on phrase hover
+		clickChange: false,                                 // Change phrase on click
+		loopCount: 'infinite',                              // Loop Count - 'infinite' or number
+		autoRun: true,                                      // Run rotator automatically
+		onInit: false,                                      // Function
+		onChange: false,                                    // Function
+		onComplete: false                                   // Function
+	});
+}
+
+
+/* Card Slider - Swiper */
+var cardSlider = new Swiper('.card-slider', {
+	autoplay: {
+		delay: 4000,
+		disableOnInteraction: false
+	},
+	loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	slidesPerView: 3,
+	spaceBetween: 70,
+	breakpoints: {
+		// when window is <= 767px
+		767: {
+			slidesPerView: 1
+		},
+		// when window is <= 991px
+		991: {
+			slidesPerView: 2,
+			spaceBetween: 40
+		}
+	}
+});
+
+
+/* Filter - Isotope */
+const gridCheck = document.querySelector('.grid');
+
+if (gridCheck !== null) { 
+	// init Isotope
+	var iso = new Isotope( '.grid', {
+		itemSelector: '.element-item',
+		layoutMode: 'fitRows'
+	});
+
+	// bind filter button click
+	var filtersElem = document.querySelector('.filters-button-group');
+	filtersElem.addEventListener( 'click', function( event ) {
+		// only work with buttons
+		if ( !matchesSelector( event.target, 'button' ) )  {
+			return;
+		}
+		var filterValue = event.target.getAttribute('data-filter');
+		// use matching filter function
+		iso.arrange({ filter: filterValue });
+	});
+	
+	// change is-checked class on buttons
+	var buttonGroups = document.querySelectorAll('.button-group');
+	for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
+		var buttonGroup = buttonGroups[i];
+		radioButtonGroup( buttonGroup );
+	}
+	
+	function radioButtonGroup( buttonGroup ) {
+		buttonGroup.addEventListener( 'click', function( event ) {
+			// only work with buttons
+			if ( !matchesSelector( event.target, 'button' ) )  {
+				return;
+			}
+			buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
+			event.target.classList.add('is-checked');
+		});
+	}
+}
+
+
+/* Back To Top Button */
+// Get the button
+myButton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+function scrollFunctionBTT() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		myButton.style.display = "block";
+	} else {
+		myButton.style.display = "none";
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0; // for Safari
+	document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE and Opera
+}
+
+function toggleDisplay() {
+	const option = document.getElementById("inputGroupSelect1").value;
+	const jobs = document.getElementById("jobs-input");
+
+	if (option === "others") {
+		jobs.style.display = "block";
+	} else jobs.style.display = "hidden";
+}
+
+
+// File upload
+const form = document.querySelector(".upload-wrapper"),
+fileInput = document.querySelector(".file-input"),
+progressArea = document.querySelector(".progress-area"),
+uploadedArea = document.querySelector(".uploaded-area");
+
+// form click event
+form.addEventListener("click", () =>{
+  fileInput.click();
+});
+
+fileInput.onchange = ({target})=>{
+  let file = target.files[0]; //getting file [0] this means if user has selected multiple files then get first one only
+  if(file){
+    let fileName = file.name; //getting file name
+    if(fileName.length >= 12){ //if file name length is greater than 12 then split it and add ...
+      let splitName = fileName.split('.');
+      fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
+    }
+    uploadFile(fileName); //calling uploadFile with passing file name as an argument
+  }
+}
+
+// file upload function
+function uploadFile(name){
+  let xhr = new XMLHttpRequest(); //creating new xhr object (AJAX)
+  xhr.open("POST", "php/upload.php"); //sending post request to the specified URL
+  xhr.upload.addEventListener("progress", ({loaded, total}) =>{ //file uploading progress event
+    let fileLoaded = Math.floor((loaded / total) * 100);  //getting percentage of loaded file size
+    let fileTotal = Math.floor(total / 1000); //gettting total file size in KB from bytes
+    let fileSize;
+    // if file size is less than 1024 then add only KB else convert this KB into MB
+    (fileTotal < 1024) ? fileSize = fileTotal + " KB" : fileSize = (loaded / (1024*1024)).toFixed(2) + " MB";
+    let progressHTML = `<li class="row">
+                          <i class="fas fa-file-alt"></i>
+                          <div class="content">
+                            <div class="details">
+                              <span class="name">${name} • Uploading</span>
+                              <span class="percent">${fileLoaded}%</span>
+                            </div>
+                            <div class="progress-bar">
+                              <div class="progress" style="width: ${fileLoaded}%"></div>
+                            </div>
+                          </div>
+                        </li>`;
+    // uploadedArea.innerHTML = ""; //uncomment this line if you don't want to show upload history
+    uploadedArea.classList.add("onprogress");
+    progressArea.innerHTML = progressHTML;
+    if(loaded == total){
+      progressArea.innerHTML = "";
+      let uploadedHTML = `<li class="row">
+                            <div class="content upload">
+                              <i class="fas fa-file-alt"></i>
+                              <div class="details">
+                                <span class="name">${name} • Uploaded</span>
+                                <span class="size">${fileSize}</span>
+                              </div>
+                            </div>
+                            <i class="fas fa-check"></i>
+                          </li>`;
+      uploadedArea.classList.remove("onprogress");
+      // uploadedArea.innerHTML = uploadedHTML; //uncomment this line if you don't want to show upload history
+      uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML); //remove this line if you don't want to show upload history
+    }
+  });
+  let data = new FormData(form); //FormData is an object to easily send form data
+  xhr.send(data); //sending form data
+}
